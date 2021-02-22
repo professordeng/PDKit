cf3354dffd9d9912adcd48c130e77b87db3adcdc
professordeng <2248262060@qq.com>
2021-02-22 10:36:14 +0800

    init framework

---
+++ PDKit/PDKit/PDKit.h
@@ -0,0 +1,18 @@
+//
+//  PDKit.h
+//  PDKit
+//
+//  Created by deng on 2021/2/22.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for PDKit.
+FOUNDATION_EXPORT double PDKitVersionNumber;
+
+//! Project version string for PDKit.
+FOUNDATION_EXPORT const unsigned char PDKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PDKit/PublicHeader.h>
+
+
---
+++ PDKit/PDKit/Service.swift
@@ -0,0 +1,18 @@
+//
+//  Service.swift
+//  PDKit
+//
+//  Created by deng on 2021/2/22.
+//
+
+import Foundation
+
+public class Service {
+    
+    private init() {}
+    
+    public static var name: String {
+        return "Service name"
+    }
+    
+}
