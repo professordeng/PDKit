633b107602ca6de10a570d80059ba341b41a954c
professordeng <2248262060@qq.com>
2021-02-28 17:44:54 +0800

    feat: 动画素材

--- TestPDKit/TestPDKit/ViewController.swift
+++ TestPDKit/TestPDKit/ViewController.swift
@@ -10,34 +10,69 @@ import PDKit
 
 class ViewController: UIViewController {
     
-    let colorView = UIView()
+    let startView: UIView = .init()
+    let endView  : UIView = .init()
+    
     var displayLink: DisplayLink?
+    var status: Status = .init()
+    let xOffset: CGFloat = 0
+    let yOffset: CGFloat = 100
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        colorView.make {
+        startView.make {
             view.addSubview($0)
             $0.backgroundColor = Color.green
             $0.layer.cornerRadius = 50
         }.al.make {
-            $0.centerX(view.al.centerX)
-            $0.centerY(view.al.centerY)
             $0.size(100)
+            $0.centerToSuperview()
         }
         
-        var sum = 0
-        displayLink = .init(60) {
-            print("Hello")
-            sum += 1
-            if sum == 5 { self.displayLink = nil }
+        endView.make {
+            view.addSubview($0)
+            $0.layer.borderWidth = 5
+            $0.layer.borderColor = Color.red.cgColor
+            $0.layer.cornerRadius = 50
+        }.al.make {
+            $0.size(100)
+            $0.centerXToSuperview()
+            $0.centerY(view.al.centerY, 100)
         }
         
     }
     
+    func move(duration: Int, fps: Int) {
+        let xStep = self.xOffset / CGFloat(duration * fps)
+        let yStep = self.yOffset / CGFloat(duration * fps)
+        
+        var sum: Int = 0
+        displayLink = .init(fps) {
+            sum += 1
+            if sum >= (duration*fps) { self.displayLink = nil }
+            
+            self.status.move(xStep: xStep, yStep: yStep)
+            self.startView.al.make {
+                $0.updateCenterX(self.status.xOffset)
+                $0.updateCenterY(self.status.yOffset)
+            }
+        }
+    }
+    
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        colorView.al.make {
-            $0.updateCenterY(100)
+        move(duration: 1, fps: 60)
+    }
+}
+
+extension ViewController {
+    struct Status {
+        var xOffset: CGFloat = 0
+        var yOffset: CGFloat = 0
+        
+        mutating func move(xStep: CGFloat, yStep: CGFloat) {
+            xOffset += xStep
+            yOffset += yStep
         }
     }
 }
